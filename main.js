752c9e0f1db76483f5a77034aed06c58f9b4ffb4
Tim <der.beste3424@gmail.com>
2024-07-28 18:11:13 +0200

    test

--- main.js
+++ main.js
@@ -9,6 +9,39 @@ var adapter = utils.Adapter(adaptername);
 var IP = "0.0.0.0"; //HoyMiles IP address
 var TIMING = 1; //Request timing
 
+// Funktion zum Erstellen von Datenpunkten und Ordnern
+function createDataPoints(adapter, basePath, data) {
+    if (Array.isArray(data)) {
+        data.forEach((value, index) => {
+            const id = `${basePath}.${index}`;
+            adapter.setObjectNotExists(id, {
+                type: 'state',
+                common: {
+                    name: `${basePath} ${index}`,
+                    type: 'string',
+                    role: 'value',
+                    read: true,
+                    write: true
+                },
+                native: {}
+            });
+            adapter.setState(id, value);
+        });
+    } else if (typeof data === 'object') {
+        Object.keys(data).forEach(key => {
+            const newPath = `${basePath}.${key}`;
+            adapter.setObjectNotExists(newPath, {
+                type: 'channel',
+                common: {
+                    name: key
+                },
+                native: {}
+            });
+            createDataPoints(adapter, newPath, data[key]);
+        });
+    }
+}
+
 adapter.on('ready', function() {
     adapter.log.info("Gestartet");
 
@@ -16,4 +49,20 @@ adapter.on('ready', function() {
     TIMING = adapter.config.requesttiming;
 
     adapter.log.info("IP: " + IP + "; Timing: " + TIMING);
+
+    jsonData = require("./data.json")
+    
+    // Hauptordner dynamisch erstellen
+    Object.keys(jsonData).forEach(key => {
+        const basePath = key;
+        adapter.setObjectNotExists(basePath, {
+            type: 'channel',
+            common: {
+                name: key
+            },
+            native: {}
+        });
+        createDataPoints(adapter, basePath, jsonData[key]);
+    });
+    
 });
